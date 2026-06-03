a1ab5bd5b7f3b83db326669ab821c39242920383
Pap0m <enrique.rios@iteso.mx>
2026-06-02 22:27:53 -0600

    start another vault project but now using c and zig for the backend

---
+++ src/main.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+const Io = std.Io;
+const c = @cImport({
+    @cInclude("stdio.h");
+    @cInclude("mongoose.h");
+    @cInclude("sqlite3.h");
+});
+
+const Bulwark = @import("Bulwark");
+
+pub fn ev_handler(conn: ?*c.mg_connection, ev: c_int, ev_data: ?*anyopaque) callconv(.c) void {
+    if (ev == c.MG_EV_HTTP_MSG) { // new http request received
+        const hm: *c.mg_http_message = @ptrCast(@alignCast(ev_data));
+
+        if (c.mg_match(hm.uri, c.mg_str("/#"), null)) {
+            const opts: c.mg_http_serve_opts = .{ .root_dir = "./public", .fs = &c.mg_fs_posix };
+            c.mg_http_serve_dir(conn, hm, &opts);
+        } else {
+            c.mg_http_reply(conn, 404, "", "%s", "Not Found\n");
+        }
+        // routes that needs authentication
+        if (c.mg_match(hm.uri, c.mg_str("/api/#"), null)) {
+            const auth_header = c.mg_http_get_header(hm, "Authorization") orelse {
+                c.mg_http_reply(conn, 401, "Content-Type: application/json\r\n", "{\"error\": \"Unauthorized\"}\n");
+                return;
+            };
+
+            _ = auth_header;
+        }
+
+        // auth routes
+        // GET login
+        if (c.mg_match(hm.method, c.mg_str("GET"), null) and c.mg_match(hm.uri, c.mg_str("/login"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"login\"}\n");
+        }
+        // POST login
+        if (c.mg_match(hm.method, c.mg_str("POST"), null) and c.mg_match(hm.uri, c.mg_str("/login"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"login\"}\n");
+        }
+
+        // GET register
+        if (c.mg_match(hm.method, c.mg_str("GET"), null) and c.mg_match(hm.uri, c.mg_str("/register"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"register\"}\n");
+        }
+        // POST register
+        if (c.mg_match(hm.method, c.mg_str("POST"), null) and c.mg_match(hm.uri, c.mg_str("/register"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"register\"}\n");
+        }
+
+        // vault items
+        // GET dashboard
+        if (c.mg_match(hm.method, c.mg_str("GET"), null) and c.mg_match(hm.uri, c.mg_str("/dashboard"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"dashboard\"}\n");
+        }
+
+        // GET items
+        if (c.mg_match(hm.method, c.mg_str("GET"), null) and c.mg_match(hm.uri, c.mg_str("/items"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"items\"}\n");
+        }
+        // GET item form
+        if (c.mg_match(hm.method, c.mg_str("GET"), null) and c.mg_match(hm.uri, c.mg_str("/items/new"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"items/new\"}\n");
+        }
+        // POST new item
+        if (c.mg_match(hm.method, c.mg_str("POST"), null) and c.mg_match(hm.uri, c.mg_str("/items"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"items\"}\n");
+        }
+
+        // show independent items
+        // GET /items/:id
+        // GET /items/:id/edit
+        // PUT /items/:id
+        // DELETE /items/:id
+
+        // session managment
+        // POST logout
+        if (c.mg_match(hm.method, c.mg_str("POST"), null) and c.mg_match(hm.uri, c.mg_str("/logout"), null)) {
+            c.mg_http_reply(conn, 200, "Content-Type: application/json\r\n", "{\"route\": \"logout\"}\n");
+        }
+    }
+}
+
+pub fn main() !void {
+    var mgr: c.mg_mgr = .{};
+    c.mg_mgr_init(&mgr);
+    defer c.mg_mgr_free(&mgr);
+
+    _ = c.mg_http_listen(&mgr, "http://0.0.0.0:8000", ev_handler, null);
+
+    while (true) {
+        c.mg_mgr_poll(&mgr, 1000);
+    }
+}
